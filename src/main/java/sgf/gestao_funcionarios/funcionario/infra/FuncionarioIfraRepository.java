2927ed92a07726cea7241e4688a531d706c0cd71
Artur Carvalho <arturpedroso5050@gmail.com>
2024-11-23 19:17:19 -0300

    implementa cliente repository

--- src/main/java/sgf/gestao_funcionarios/funcionario/infra/FuncionarioIfraRepository.java
+++ src/main/java/sgf/gestao_funcionarios/funcionario/infra/FuncionarioIfraRepository.java
@@ -1,19 +1,23 @@
 package sgf.gestao_funcionarios.funcionario.infra;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import sgf.gestao_funcionarios.funcionario.application.repository.FuncionarioRepository;
 import sgf.gestao_funcionarios.funcionario.domain.Funcionario;
 
 @Repository
 @Log4j2
+@RequiredArgsConstructor
 public class FuncionarioIfraRepository implements FuncionarioRepository {
+    private final FuncionarioSpringDataJPARepository funcionarioSpringDataJPARepository;
 
     @Override
     public Funcionario salva(Funcionario funcionario){
-        log.info("[inicia] FuncionarioIfraRepository - salva ");
+        log.info("[inicia] FuncionarioIfraRepository - salva");
         funcionarioSpringDataJPARepository.save(funcionario);
-        log.info("[finaliza] FuncionarioIfraRepository - salva ");
+        log.info("[finaliza] FuncionarioIfraRepository - salva");
         return funcionario;
     }
 }
