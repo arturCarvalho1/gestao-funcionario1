f98c4955e568c51a38040579f44d70561871c198
Artur Carvalho <arturpedroso5050@gmail.com>
2024-11-23 17:07:16 -0300

    implementa controller e fluxo para service

--- src/main/java/sgf/gestao_funcionarios/funcionario/application/api/FuncionarioController.java
+++ src/main/java/sgf/gestao_funcionarios/funcionario/application/api/FuncionarioController.java
@@ -1,15 +1,21 @@
 package sgf.gestao_funcionarios.funcionario.application.api;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
+import sgf.gestao_funcionarios.funcionario.application.service.FuncionarioService;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor
 public class FuncionarioController implements FuncionarioAPI {
+    private final FuncionarioService funcionarioService;
+
     @Override
     public FuncionarioResponse postFuncionario(FuncionarioRequest funcionarioRequest){
        log.info("[inicia] FuncionarioController - postFuncionario");
+       FuncionarioResponse funcionarioCriado = funcionarioService.criaFuncionario(funcionarioRequest);
        log.info("[finaliza] FuncionarioController - postFuncionario");
-        return null;
+        return funcionarioCriado;
     }
 }
--- src/main/java/sgf/gestao_funcionarios/funcionario/application/service/FuncionarioService.java
+++ src/main/java/sgf/gestao_funcionarios/funcionario/application/service/FuncionarioService.java
@@ -1,4 +1,8 @@
 package sgf.gestao_funcionarios.funcionario.application.service;
 
+import sgf.gestao_funcionarios.funcionario.application.api.FuncionarioRequest;
+import sgf.gestao_funcionarios.funcionario.application.api.FuncionarioResponse;
+
 public interface FuncionarioService {
+    FuncionarioResponse criaFuncionario(FuncionarioRequest funcionarioRequest);
 }
