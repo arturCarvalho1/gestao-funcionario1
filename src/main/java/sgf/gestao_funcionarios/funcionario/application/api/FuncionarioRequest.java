528cd1fc73f8cba2670881b4ebf03cc6ee238ff1
Artur Carvalho <arturpedroso5050@gmail.com>
2024-11-23 17:11:51 -0300

    implementa request e response

--- src/main/java/sgf/gestao_funcionarios/funcionario/application/api/FuncionarioRequest.java
+++ src/main/java/sgf/gestao_funcionarios/funcionario/application/api/FuncionarioRequest.java
@@ -1,4 +1,15 @@
 package sgf.gestao_funcionarios.funcionario.application.api;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+
+@Value
 public class FuncionarioRequest {
+    @NotBlank
+    private String name;
+    private String designacao;
+    private double salario;
+    @NotBlank
+    private String telefone;
+    private String endereco;
 }
--- src/main/java/sgf/gestao_funcionarios/funcionario/application/api/FuncionarioResponse.java
+++ src/main/java/sgf/gestao_funcionarios/funcionario/application/api/FuncionarioResponse.java
@@ -1,4 +1,9 @@
 package sgf.gestao_funcionarios.funcionario.application.api;
 
+import lombok.Value;
+
+import java.util.UUID;
+@Value
 public class FuncionarioResponse {
+    private UUID idFuncionario;
 }
