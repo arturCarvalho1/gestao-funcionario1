87142192ac86d916932724f197a4544a593d051d
Artur Carvalho <arturpedroso5050@gmail.com>
2024-11-24 14:00:26 -0300

    implementa getTodos

--- src/main/java/sgf/gestao_funcionarios/funcionario/application/api/FuncionarioListResponse.java
+++ src/main/java/sgf/gestao_funcionarios/funcionario/application/api/FuncionarioListResponse.java
@@ -1,10 +1,13 @@
 package sgf.gestao_funcionarios.funcionario.application.api;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
 import sgf.gestao_funcionarios.funcionario.domain.Funcionario;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Value
 public class FuncionarioListResponse {
     private String name;
     private String designacao;
@@ -13,6 +16,18 @@ public class FuncionarioListResponse {
     private String endereco;
 
     public static List<FuncionarioListResponse> converte(List<Funcionario> funcionarios) {
-        return null;
+        return funcionarios.stream()
+                .map(FuncionarioListResponse::new)
+                .collect(Collectors.toList());
+
+    }
+
+    public FuncionarioListResponse(Funcionario funcionario) {
+        this.designacao = funcionario.getDesignacao();
+        this.endereco = funcionario.getEndereco();
+        this.name = funcionario.getName();
+        this.salario = funcionario.getSalario();
+        this.telefone = funcionario.getTelefone();
     }
 }
+
